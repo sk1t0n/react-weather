3e264c9bd8807b7300cb2de4b5fe87c8cda65448
Anton Grabovsky <agrabovsky2020@gmail.com>
2021-10-14 18:34:35 +0300

    Show the temperature after receiving a request from the server

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,19 +1,71 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import FormBootstrap from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import styles from './Form.module.css';
+import { Result } from '../Result';
 
 function Form(props) {
+  const API_KEY = process.env['REACT_APP_WEATHER_KEY'];
+  const url = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=`;
+  const errorMessages = {
+    '400': 'City not found, maybe you entered the wrong city name. Correct the error and try again.'
+  };
+
+  let [isLoading, setLoading] = useState(false);
+  let [temperature, setTemperature] = useState(null);
+  let [error, setError] = useState(null);
+  let inputRef = useRef();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    setLoading(true);
+    setError(null);
+    setTemperature(null);
+    
+    let value = inputRef.current.value;
+    if (value.length > 0) {
+      try {
+        const response = await fetch(url+value);
+
+        if (!response.ok) {
+          setError(errorMessages[response.status.toString()]);
+          setLoading(false);
+        } else {
+          const json = await response.json();
+          const temp_c = json.current?.temp_c;
+          const temp_f = json.current?.temp_f;
+          setTemperature({ temp_c, temp_f });
+          setLoading(false);
+        }
+      } catch(e) {
+        setError(e.message);
+        setLoading(false);
+      }
+    } else {
+      setLoading(false);  
+    }
+  }
+
+  function handleReset() {
+    setError(null);
+    setTemperature(null);
+  }
+
   return (
     <FormBootstrap className={styles.Form}>
       <FormBootstrap.Group className="mb-3" controlId="city">
         <FormBootstrap.Label>City</FormBootstrap.Label>
-        <FormBootstrap.Control type="text" placeholder="Enter city" />
+        <FormBootstrap.Control ref={inputRef} type="text" placeholder="Enter city" />
       </FormBootstrap.Group>
 
-      <Button variant="primary" type="submit">
+      <Button onClick={handleSubmit} variant="primary" type="submit">
         Submit
       </Button>
+      <Button onClick={handleReset} variant="warning" className="mx-3" type="reset">
+        Reset
+      </Button>
+      
+      <Result isLoading={isLoading} error={error} temperature={temperature} />
     </FormBootstrap>
   );
 }
---
+++ src/components/Result.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Loader } from './Loader/Loader';
+import Alert from 'react-bootstrap/Alert';
+
+function Result(props) {
+  const { isLoading, error, temperature } = props;
+
+  return (
+    <div className="mt-5">
+      { isLoading ? <Loader/> : null }
+      
+      { error !== null ? (
+        <Alert variant="danger">{ error }</Alert>
+      ) : null }
+      
+      { temperature !== null ? (
+        <Alert variant="info">
+          <p>Temperature in degrees: 
+            <span className="float-end">{ temperature.temp_c } &#8451;</span>
+          </p>
+          <p>Temperature in fahrenheit: 
+            <span className="float-end">{ temperature.temp_f } &#8457;</span>
+          </p>
+        </Alert>
+      ) : null }
+    </div>
+  );
+}
+
+export { Result };
